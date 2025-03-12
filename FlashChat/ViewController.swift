c0c8a939d4b70650847fc793492e315307b487f4
nguyenhau2003dz <nguyenduchaudeptrai2003@gmail.com>
2025-03-12 13:14:51 +0700

    Initial Commit

---
+++ FlashChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Hậu Nguyễn on 12/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FlashChatTests/FlashChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlashChatTests.swift
+//  FlashChatTests
+//
+//  Created by Hậu Nguyễn on 12/3/25.
+//
+
+import Testing
+@testable import FlashChat
+
+struct FlashChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
